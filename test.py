c3b946629456d5b809bd3d233ae56bceb8b793a1
Guillaume SCHMIRGAL <g.schmirgal@gmail.com>
2025-05-29 10:28:28 +0200

    add requirements.txt and update test.py

--- test.py
+++ test.py
@@ -18,6 +18,9 @@ def fetch_and_parse_json(url):
     except ValueError as e:
         print(f"Erreur lors de la conversion en JSON : {e}")
 
+def csvify(values):
+    return '"'+'";"'.join(str(x) for x in values.values()) + '"\n'
+
 
 def create_product_csv():
     url = "https://downloads.s3.cardmarket.com/productCatalog/productList/products_singles_1.json"  
@@ -27,12 +30,12 @@ def create_product_csv():
     products = data.get("products")
 
     
-    f_products = open("products_file.csv", "w", encoding="utf-8")
+    f_products = open("csvtemp/products_file.csv", "w", encoding="utf-8")
 
     for product in products:
         product.pop("categoryName")
         product.pop("idCategory")
-        f_products.write( '"'+'";"'.join(str(x) for x in product.values()) + '"\n')
+        f_products.write( csvify(product) )
 
 
 def create_prices_csv():
@@ -48,32 +51,7 @@ def create_prices_csv():
     for priceGuide in priceGuides:
         priceGuide.pop("idCategory")
         priceGuide["date"] = createdAt
-        f_prices.write( '"'+'";"'.join(str(x) for x in priceGuide.values()) + '"\n')
-
-
-def create_expansions_csv_obsolete():
-    url = "https://www.cardmarket.com/fr/Magic/Products/Singles"
-    name = "idExpansion"
-
-    
-    f_expansions = open("csvtemp/expansions_file.csv", "w", encoding="utf-8")
-   
-    
-    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    response.raise_for_status()
-
-    plop = response.text
-
-    soup = BeautifulSoup(plop, 'html.parser')
-    select = soup.find('select', attrs={'name': name})
-    if not select:
-        return []
-    # Extraire les options du select
-    for option in select.find_all('option'):
-        value = option.get('value')
-        text = option.text.strip()
-        f_expansions.write( '"' + value + '";"' + text + '"\n')
-
+        f_prices.write( csvify( priceGuide ))
 
 def create_expansions_csv():
     url = "https://www.cardmarket.com/fr/Magic/Products/Singles"
@@ -81,8 +59,10 @@ def create_expansions_csv():
     
     f_expansions = open("csvtemp/expansions_file.csv", "w", encoding="utf-8")
 
+    options = uc.ChromeOptions()
+    options.add_argument('--user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36')
+    driver = uc.Chrome(options=options, headless=True)
 
-    driver = uc.Chrome(headless=True)
     driver.get(url)
 
     # Wait for Cloudflare challenge (increase if needed)
@@ -97,13 +77,16 @@ def create_expansions_csv():
     for option in select.find_all('option'):
         value = option.get('value')
         text = option.text.strip()
-        f_expansions.write( '"' + value + '";"' + text + '"\n')
+        #f_expansions.write( '"' + value + '";"' + text + '"\n')
+        if value != "0":
+            f_expansions.write( csvify( {1:value, 2:text} ) )
+
 
     driver.quit()
 
 
 
 
-#create_product_csv()
-#create_prices_csv()
+create_product_csv()
+create_prices_csv()
 create_expansions_csv()
